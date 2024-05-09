52b42f2fb4b28d083d5121de2bda3fa45bc795b2
Nathalia Coutinho <106166403+NathaliaCout@users.noreply.github.com>
2024-05-09 15:39:01 -0300

    M3Lab01

---
+++ M3Lab01/main.tf
@@ -0,0 +1,28 @@
+# settings block
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.102.0"
+    }
+  }
+}
+
+#provider "azurerm" {
+# Configuration options
+// features {  }
+//subscription_id = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+//client_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+//client_secret   = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+//tenant_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+//}
+
+#Module block
+module "storage_module" {
+  source = "./storage_account"
+}
+
+#Outputs block
+output "storage_account_id" {
+  value = module.storage_module.storage_account_id
+}
